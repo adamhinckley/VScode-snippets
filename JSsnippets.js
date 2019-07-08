44238b82884830c72c550efef7ba54c7bff659eb
Adam Hinckley <40447332+adamhinckley@users.noreply.github.com>
2019-07-08 18:59:52 -0500

    init

---
+++ JSsnippets.js
@@ -0,0 +1,38 @@
+{
+    "Console Log with string description": {
+        "prefix": "cls",
+        "body": "console.log('$1', $2)",
+        "description": "console log with string "
+    },
+    "Console Log": {
+        "prefix": "clv",
+        "body": "console.log($1)",
+        "description": "console log with variable"
+    },
+    "Arrow function": {
+        "prefix": "afn",
+        "body": ["const ${1:name} = ($2) => {", "  $3", "}"],
+        "description": "Arrow function"
+    },
+    "hook": {
+        "prefix": "hook",
+        "body": ["const [$1, set$2] = React.useState($3)"],
+        "description": "hook"
+    },
+    "Import React": {
+        "prefix": "irfc",
+        "body": [
+            "import React from \"react\"",
+            "",
+            "const $1 = ($2) => {",
+            "",
+            " return (",
+            "  <$4>",
+            "  </>",
+            ")",
+            "}",
+            "export default $3"
+        ],
+        "description": "Import React Functional component"
+    }
+}
